3f9df0786268230cd6aeb7a93f90c3bf86db248a
jaclynflomen <j_floman@fanshaweonline.ca>
2018-11-30 11:40:20 -0500

    getting rid of unnecessary code

--- includes/index.php
+++ includes/index.php
@@ -11,23 +11,4 @@
         //can use this for portfolio pieces too - does not only apply to media
     }
 
-    //single tv route
-    if (isset($_GET["tv"])){
-        $data = get_all_tv($conn, $_GET["tv"]);
-        echo json_encode($data);
-    } else {
-        $data = get_all_tv($conn);
-        echo json_encode($data);
-        //can use this for portfolio pieces too - does not only apply to media
-    }
-
-    //single audio route
-    if (isset($_GET["audio"])){
-        $data = get_all_audio($conn, $_GET["audio"]);
-        echo json_encode($data);
-    } else {
-        $data = get_all_audio($conn);
-        echo json_encode($data);
-        //can use this for portfolio pieces too - does not only apply to media
-    }
 ?>
