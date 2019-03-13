ebc97a00241686953610c28519e599513636f857
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-13 18:56:44 -0400

    made components but not working

--- js/components/UserComponent.js
+++ js/components/UserComponent.js
@@ -2,6 +2,68 @@ export default {
     template: `
 <div>
 
+<router-link :to="{ path: '/home'}"><img id="logo" src="images/av-logo.svg" width="200px"></router-link>
+
+<nav role="navigation">
+        <div id="menuToggle">
+<input id="checkbox" type="checkbox"/>
+        
+<span></span>
+<span></span>
+<span></span>
+
+<ul id="menu">
+    <router-link :to="{ path: '/promotions'}"><li>EDIT PROFILE</li></router-link>
+    <router-link :to="{ path: '/home'}"><li>LOG OUT</li></router-link>
+</ul>
+
+
+</div>
+</nav>
+
+<div class="row">
+
+<!-- <img id="logo" class="col-xs-2 col-sm-4 col-md-2" src="images/av-logo.svg" alt="flashback logo" title="flashback logo"> -->
+</div>
+
+<div id="whoWatch"><h2>Who's Watching?</h2></div>
+
+<div class="row people">
+<div class="col-xs-12 col-sm-6 col-md-4 mx-auto">
+    <div @click="navToUserHome()">
+        <div class="card-body text-center">
+            <img src="images/adults.jpg" class="img-fluid" alt="user photo" title="user photo">
+            <p>Adults</p>
+        </div>
+    </div>
+</div>
+
+<div class="col-xs-12 col-sm-6 col-md-4 mx-auto">
+    <div @click="navToUserHome()">
+            <div class="card-body text-center ">
+                <img src="images/kids.jpg" class="img-fluid" alt="user photo" title="user photo">
+                <p>kids</p>
+            </div>
+    </div> 
+</div>
+</div>
+
+
+
+<footer>
+<p class="copy">
+    &copy; 2019 ROKU, INC & FLASHBACK. ALL RIGHTS RESERVED.
+</p>
+
+<ul class="float-right">
+    <li><i class="fab fa-facebook"></i></li>
+    <li><i class="fab fa-twitter-square"></i></li>
+    <li><i class="fab fa-google-plus-square"></i></li>
+    <li><i class="fab fa-instagram"></i></li>
+</ul>
+</footer>
+
+
 </div>
     `,
 
