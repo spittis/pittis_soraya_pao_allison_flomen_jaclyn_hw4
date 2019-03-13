bd3dec0095f2f5db019d0ae37611c8dcf777b5de
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-03-13 11:09:15 -0400

    yo it works

--- admin/scripts/config.php
+++ admin/scripts/config.php
@@ -3,8 +3,4 @@ require_once('functions.php');
 require_once('read.php');
 require_once('login.php');
 require_once('sessions.php');
-<<<<<<< HEAD
 require_once('user.php');
-=======
-require_once('user.php');
->>>>>>> 166ad384b63a8b4d81793ea5f11b2b7ce4b0c6f3
